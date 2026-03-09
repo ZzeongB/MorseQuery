57a6fcc3639ac4ad61d9f2b7b26b56787315ac1d
ZzeongB <>
2026-03-09 14:31:21 -0400

    config

--- live/config.py
+++ live/config.py
@@ -30,7 +30,7 @@ OPENAI_SESSION_CONFIG = {
     "input_audio_format": "pcm16",
     "turn_detection": {
         "type": "server_vad",
-        "threshold": 0.45,
+        "threshold": 0.5,
         "prefix_padding_ms": 200,
         "silence_duration_ms": 200,
         "create_response": False,
