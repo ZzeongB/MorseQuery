748c11e89fa6ca38e55eb19bd8b48df07655a537
ZzeongB <>
2026-03-14 16:53:59 -0400

    pilot

--- live/_parts/web_realtime/socket_handlers.py
+++ live/_parts/web_realtime/socket_handlers.py
@@ -1093,7 +1093,7 @@ if __name__ == "__main__":
     sio.run(
         app,
         host="0.0.0.0",
-        port=5001,
+        port=5003,
         debug=False,
         allow_unsafe_werkzeug=True,
     )
